4e9777bf9eba1f3d3621842b8d37bc8a5adfb005
Dawood Sardar <dawoodsardar252@gmail.com>
2026-08-09 22:37:54 +0500

    feat: implement login functionality with form handling and user authentication

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import { loginUser } from "../services/authService";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const data = await loginUser(email, password);
+
+      console.log(data);
+
+      alert("Login Successfully");
+    } catch (error) {
+      console.log(error);
+
+      alert("Login failed");
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
