a029422fc273e049a13f32d6046f0cfc386f12c3
Dawood Sardar <dawoodsardar252@gmail.com>
2026-08-10 08:40:43 +0500

    feat: update Desktop component layout and add Taskbar, StartMenu, and Clock components

---
+++ client/src/components/taskbar/Taskbar.tsx
@@ -0,0 +1,20 @@
+export default function Taskbar() {
+  return (
+    <div
+      className="
+        absolute
+        bottom-0
+        left-0
+        w-full
+        h-12
+        bg-black/40
+        backdrop-blur-md
+        border-t
+        border-white/10
+        text-black
+      "
+    >
+      Taskbar
+    </div>
+  );
+}
--- client/src/pages/Desktop.tsx
+++ client/src/pages/Desktop.tsx
@@ -1,7 +1,26 @@
+import Taskbar from "../components/taskbar/Taskbar";
+
 export default function Desktop() {
   return (
-    <div>
-      <h1>Welcome to Dawvix OS</h1>
-    </div>
+    <>
+      <div
+        className="
+        h-screen
+        w-screen
+        overflow-hidden
+        relative
+        bg-slate-900
+        text-gray
+        text-center
+      "
+        style={{
+          backgroundImage:
+            "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb')",
+        }}
+      >
+        Desktop
+      </div>
+      <Taskbar />
+    </>
   );
 }
